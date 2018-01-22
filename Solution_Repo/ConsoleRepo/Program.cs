bdfaf79034a2db60de5ba5b67e8a5b023de283b9
studenti <studenti@LAB2-PC-13>
2018-01-22 09:22:25 +0100

    Programma eseguito in classe

---
+++ Solution_Repo/ConsoleRepo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scuola = "";
+            string programma = "";
+
+            Console.WriteLine("Inserisci il nome della scuola");
+            scuola = Console.ReadLine();
+            Console.WriteLine("Inserisci il nome del programma");
+            programma = Console.ReadLine();
+
+        }
+    }
+}
